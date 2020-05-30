19a30937d9152387438e040d586974d21e6073e0
Quentin Fontaine <21800691@etu.unicaen.fr>
2020-05-30 15:51:56 +0200

    trouver un resto

--- src/login/LoginClient.js
+++ src/login/LoginClient.js
@@ -16,6 +16,12 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import firebase from "firebase";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -60,13 +66,78 @@ function Copyright() {
     );
 }
 
-const LoginRestaurateur = ({ history }) => {
+const LoginClient = ({ history }) => {
+
+
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const [infoPopUp, setInfoPopUp] = React.useState({
+        titre: "",
+        desc: "",
+        valid: false,
+    });
+    const [docId, setDocId] = React.useState("");
+
+    const handleClickOpen = () => {
+        setOpenDialog(true);
+    };
+
+    const handleClose = () => {
+        setOpenDialog(false);
+    };
+
+    const allerPageResto = () => {
+        history.push("/test/"+docId);
+    }
+
+    const getBoutonValid = () => {
+      return infoPopUp.valid ?
+          <Button onClick={allerPageResto} color="primary" autoFocus>
+            Oui
+          </Button> : null
+    };
+
     const handleLoginClient = useCallback(
         async event => {
             event.preventDefault();
             const { code_resto } = event.target.elements;
             const adresse = "/dashboard/"+code_resto.value;
-            history.push(adresse);
+
+            console.log(code_resto.value);
+
+            var res = false;
+            var result;
+
+            await firebase.firestore().collection("restaurant").where("code_resto", "==", code_resto.value)
+                .get()
+                .then(function(querySnapshot) {
+                    querySnapshot.forEach(function(doc) {
+                        res = true;
+                        result = doc.data();
+                        setDocId(doc.id);
+                    });
+                })
+                .catch(function(error) {
+                    console.log("Error getting documents: ", error);
+                });
+
+            if(res){
+                console.log(docId);
+                setInfoPopUp({
+                    titre: result.nom+", "+result.adresse.ville,
+                    desc: "Voulez vous consultez la carte de ce restaurant ?",
+                    valid: true
+                });
+
+            }else{
+                console.log("pas de résultat");
+                setInfoPopUp({
+                    titre: "Impossible de trouver le restaurant",
+                    desc: "Ce code ne correspond à aucun restaurant",
+                    valid: false
+                });
+            }
+            handleClickOpen();
+
         },
         [history]
     );
@@ -89,64 +160,88 @@ const LoginRestaurateur = ({ history }) => {
     const classes = useStyles();
 
     return (
-        <Grid container component="main" className={classes.root}>
-            <CssBaseline />
-            <Grid item xs={false} sm={4} md={7} className={classes.image} />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Passer une commande
-                    </Typography>
-                    <form className={classes.form} onSubmit={handleLoginClient}>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            inputProps={{
-                                maxLength: CHARACTER_LIMIT
-                            }}
-                            helperText={`${values.name.length}/${CHARACTER_LIMIT}`}
-                            onChange={handleChange("name")}
-                            required
-                            fullWidth
-                            id="code_resto"
-                            label="Code restaurant"
-                            name="code_resto"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Accéder au restaurant
-                        </Button>
+        <>
+            <Grid container component="main" className={classes.root}>
+                <CssBaseline />
+                <Grid item xs={false} sm={4} md={7} className={classes.image} />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Passer une commande
+                        </Typography>
+                        <form className={classes.form} onSubmit={handleLoginClient}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                inputProps={{
+                                    maxLength: CHARACTER_LIMIT
+                                }}
+                                helperText={`${values.name.length}/${CHARACTER_LIMIT}`}
+                                onChange={handleChange("name")}
+                                required
+                                fullWidth
+                                id="code_resto"
+                                label="Code restaurant"
+                                name="code_resto"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Accéder au restaurant
+                            </Button>
 
-                        <Box mt={4}>
-                            <Link href="/signup">
-                                <Button
-                                    type="button"
-                                    fullWidth
-                                    variant="contained"
-                                    className={classes.submit}
-                                >
-                                    Je suis un restaurateur
-                                </Button>
-                            </Link>
-                        </Box>
-                        <Box mt={5}>
-                            <Copyright />
-                        </Box>
-                    </form>
-                </div>
+                            <Box mt={4}>
+                                <Link href="/signup">
+                                    <Button
+                                        type="button"
+                                        fullWidth
+                                        variant="contained"
+                                        className={classes.submit}
+                                    >
+                                        Je suis un restaurateur
+                                    </Button>
+                                </Link>
+                            </Box>
+                            <Box mt={5}>
+                                <Copyright />
+                            </Box>
+                        </form>
+                    </div>
+                </Grid>
             </Grid>
-        </Grid>
+
+            <Dialog
+                open={openDialog}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                >
+                <DialogTitle id="alert-dialog-title">{infoPopUp.titre}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {infoPopUp.desc}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="secondary">
+                        Retour
+                    </Button>
+
+                    {getBoutonValid()}
+
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
-export default withRouter(LoginRestaurateur);
+export default withRouter(LoginClient);
