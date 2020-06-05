3038a1a0a9fa9ba888f7a1540120538383f19a48
Quentin Fontaine <qfon.dev@gmail.com>
2020-06-05 14:44:04 +0200

    création menu

--- src/DashboardRestaurateur/menu/PageAjoutMenu.js
+++ src/DashboardRestaurateur/menu/PageAjoutMenu.js
@@ -13,6 +13,9 @@ import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 import Switch from "@material-ui/core/Switch";
+import TextField from "@material-ui/core/TextField";
+import ListeComposentMenu from "./ListeComposentsMenu";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
@@ -27,6 +30,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PageAjoutMenu({handleClose}) {
 
+    const [listeEntrees, setListeEntrees] = React.useState([]);
+    const [listePlats, setListePlats] = React.useState([]);
+    const [listeDesserts, setListeDesserts] = React.useState([]);
+    const [listeBoissons, setListeBoissons] = React.useState([]);
+
+    const getListElements = (type) => {
+        if (type === "entrees" && state.entrees){
+            return <ListeComposentMenu listeCompo={listeEntrees} txtBtn="une entrée"/>
+        }
+        if (type === "plats" && state.plats){
+            return <ListeComposentMenu listeCompo={listePlats} txtBtn="un plat"/>
+        }
+        if (type === "desserts" && state.desserts){
+            return <ListeComposentMenu listeCompo={listeDesserts} txtBtn="un dessert"/>
+        }
+        if (type === "boissons" && state.boissons){
+            return <ListeComposentMenu listeCompo={listeBoissons} txtBtn="une boisson"/>
+        }
+    };
+
     const classes = useStyles();
 
     const [state, setState] = React.useState({
@@ -35,6 +58,8 @@ export default function PageAjoutMenu({handleClose}) {
         desserts: false,
         boissons: false,
     });
+
+    const [nomMenu, setNomMenu] = React.useState("");
 
 
     const handleChange = (event) => {
@@ -54,25 +79,78 @@ export default function PageAjoutMenu({handleClose}) {
                 </Toolbar>
             </AppBar>
 
+            <TextField
+                required
+                variant="outlined"
+                autoFocus
+                margin="dense"
+                name="nom"
+                id="nom"
+                label="Nom du menu"
+                type="nom"
+                value={nomMenu}
+                onChange={(e) => setNomMenu(e.target.value)}
+            />
+
             <List>
                 <ListItem>
-                    <Typography variant="h5">Entrée</Typography>
-                    <Switch
-                        checked={state.entrees}
-                        onChange={handleChange}
-                        name="entrees"
-                        inputProps={{ 'aria-label': 'primary checkbox' }}
-                    />
+                    <List>
+                        <FormControlLabel
+                            control={<Switch
+                                checked={state.entrees}
+                                onChange={handleChange}
+                                name="entrees"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />}
+                            label={<Typography variant="h5">Entrée</Typography>}/>
+
+                        {getListElements("entrees")}
+                    </List>
                 </ListItem>
                 <Divider />
                 <ListItem>
-                    <Typography variant="h5">Plat</Typography>
-                    <Switch
-                        checked={state.plats}
-                        onChange={handleChange}
-                        name="plats"
-                        inputProps={{ 'aria-label': 'primary checkbox' }}
-                    />
+                    <List>
+                        <FormControlLabel
+                            control={<Switch
+                                checked={state.plats}
+                                onChange={handleChange}
+                                name="plats"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />}
+                            label={<Typography variant="h5">Plat</Typography>}/>
+
+                        {getListElements("plats")}
+                    </List>
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <List>
+                        <FormControlLabel
+                            control={<Switch
+                                checked={state.desserts}
+                                onChange={handleChange}
+                                name="desserts"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />}
+                            label={<Typography variant="h5">Déssert</Typography>}/>
+
+                        {getListElements("desserts")}
+                    </List>
+                </ListItem>
+                <Divider />
+                <ListItem>
+                    <List>
+                        <FormControlLabel
+                            control={<Switch
+                                checked={state.boissons}
+                                onChange={handleChange}
+                                name="boissons"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />}
+                            label={<Typography variant="h5">Boisson</Typography>}/>
+
+                        {getListElements("boissons")}
+                    </List>
                 </ListItem>
             </List>
         </>
