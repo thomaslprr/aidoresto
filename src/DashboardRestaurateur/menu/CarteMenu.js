e201ffddb1c6e199b976b5b04d4b16a4222c9527
Quentin Fontaine <qfon.dev@gmail.com>
2020-06-07 12:01:34 +0200

    affichage icone consulter

--- src/DashboardRestaurateur/menu/CarteMenu.js
+++ src/DashboardRestaurateur/menu/CarteMenu.js
@@ -5,7 +5,8 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Grid from "@material-ui/core/Grid";
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 
 const useStyles = makeStyles({
@@ -29,13 +30,23 @@ export default function SimpleCardMenu({menu}) {
 
     const classes = useStyles();
 
+
     return (
 
         <Card className={classes.root}>
             <CardContent>
-                <Typography variant="h5" component="h2">
-                    {menu.nom}
-                </Typography>
+                <Grid container spacing={1}>
+                    <Grid item xs={10}>
+                        <Typography variant="h5" component="h2">
+                            {menu.nom}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={2} alignContent="center">
+                        <Button color="primary">
+                            <VisibilityIcon/>
+                        </Button>
+                    </Grid>
+                </Grid>
                 <Typography className={classes.pos} color="textSecondary">
                     {menu.prix} €
                 </Typography>
