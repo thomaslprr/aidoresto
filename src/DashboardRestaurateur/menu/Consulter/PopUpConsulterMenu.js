facc9029da5fab872adb5c8095630a76bd09e43f
Quentin Fontaine <qfon.dev@gmail.com>
2020-06-07 12:10:09 +0200

    avancement pop up

--- src/DashboardRestaurateur/menu/Consulter/PopUpConsulterMenu.js
+++ src/DashboardRestaurateur/menu/Consulter/PopUpConsulterMenu.js
@@ -4,6 +4,8 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 
 
@@ -14,9 +16,16 @@ export default function PopUpConsulter({menu, open, handleClose}){
     return(
         <div>
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">{menu.nom}</DialogTitle>
+                <DialogTitle id="form-dialog-title">
+                    <Typography variant="h5" component="h2">
+                        {menu.nom} -  {menu.prix} €
+                    </Typography>
+                </DialogTitle>
                 <DialogContent>
-                    {menu.prix}
+                    <Typography variant="h6" component="h2">
+                        Entrées
+                    </Typography>
+
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
