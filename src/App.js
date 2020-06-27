38349665c8bbb8d0b959dee63309871cc8a351d4
Quentin Fontaine <21800691@etu.unicaen.fr>
2020-06-27 17:55:52 +0200

    debut page commande

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {AuthProvider} from "./login/Auth";
 import LoginRestaurateur from "./login/LoginRestaurateur";
 import Dashboard from "./DashboardRestaurateur/Dashboard";
 import PageDuResto from "./commande/PageDuResto";
+import PageCommande from "./DashboardRestaurateur/commandes/pageDeLaCommande/PageCommande";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
 
                   <Route exact path="/dashboard/:id" render={props => <Dashboard {...props} type={0}/>} />
                   <Route exact path="/dashboard/:id/commandes" render={props => <Dashboard {...props} type={1} />} />
+                  <Route exact path="/dashboard/:id/commandes/:idCommande" render={props => <PageCommande {...props} type={1} />} />
                   <Route exact path="/dashboard/:id/boissons" render={props => <Dashboard {...props} type={2} />} />
                   <Route exact path="/dashboard/:id/entrees" render={props => <Dashboard {...props} type={3} />} />
                   <Route exact path="/dashboard/:id/repas" render={props => <Dashboard {...props} type={4} />} />
---
+++ src/DashboardRestaurateur/commandes/pageDeLaCommande/PageCommande.js
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react';
+import SideBar from "../../Sidebar";
+import Information from "../../info/Information";
+import {useHistory} from "react-router";
+
+
+function PageCommande({ match: {params :{id, idCommande}} , type}){
+
+    const history = useHistory();
+
+    useEffect(()=>{
+
+        if(sessionStorage.getItem('isConnected')){
+
+            if(id===sessionStorage.getItem('idResto')){
+
+                console.log("id de la commande : "+idCommande);
+
+            }else {
+
+                history.push("/dashboard/"+sessionStorage.getItem('idResto'));
+
+            }
+
+        }else{
+            history.push("/login");
+        }
+
+
+    },[]);
+
+    return(
+        <div>
+            <SideBar id={id} type={type} module={<Information id={id}/>}/>
+        </div>
+    )
+
+
+}
+
+export default PageCommande;
